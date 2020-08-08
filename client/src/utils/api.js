38216325561e9e38ae476514425f02b7676c9233
Nishant Goyal <goyalnishant>
2020-08-08 12:37:36 +0530

    add prod url for axios

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: (process.env.NODE_ENV !== 'production') ? 'http://localhost:5000' : '',
+  baseURL: (process.env.NODE_ENV !== 'production') ? 'http://localhost:5000' : 'https://node-dev-connect.herokuapp.com',
   responseType: "json"
 });
